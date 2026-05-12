74fd331a61313f23569bae5c54bab5c8673722ba
darshanprabu18 <darshanprabu01@gmail.com>
2026-05-12 11:20:45 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+
 import AppShell from "./layouts/AppShell.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import MobileNav from "./components/MobileNav.jsx";
+
 import Home from "./pages/Home.jsx";
 import Search from "./pages/Search.jsx";
 import Playlists from "./pages/Playlists.jsx";
@@ -16,28 +19,34 @@ export default function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <AppShell />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Home />} />
-          <Route path="search" element={<Search />} />
-          <Route path="playlists" element={<Playlists />} />
-          <Route path="playlists/:id" element={<PlaylistDetail />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="upload" element={<Upload />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </AnimatePresence>
+    <>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <AppShell />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Home />} />
+            <Route path="search" element={<Search />} />
+            <Route path="playlists" element={<Playlists />} />
+            <Route path="playlists/:id" element={<PlaylistDetail />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="upload" element={<Upload />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AnimatePresence>
+
+      <MobileNav />
+    </>
   );
 }
